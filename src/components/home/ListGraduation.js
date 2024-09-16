0f14a24317c61304a5a7ae30fefd4c3b1ca5e6f4
ngkhacdai <117052857+ngkhacdai@users.noreply.github.com>
2024-09-16 10:06:13 +0700

    update panigation home page

--- src/components/home/ListGraduation.js
+++ src/components/home/ListGraduation.js
@@ -9,37 +9,52 @@ const ListGraduation = ({ listProduct }) => {
   const t = useTranslations("HomePage");
   const pathName = usePathname();
   const currentLanguage = pathName.split("/")[1];
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const pageSize = 12;
+
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
+
+  // Calculate the items for the current page
+  const paginatedList = listProduct.slice(
+    (currentPage - 1) * pageSize,
+    currentPage * pageSize
+  );
+
   if (!listProduct || listProduct.length <= 0) {
     return <p className="text-center text-gray-600">{t("noProduct")}</p>;
   }
+
   return (
     <div className="w-full">
       <Row gutter={[16, 16]}>
-        {listProduct &&
-          listProduct.length > 0 &&
-          listProduct.map((item, index) => (
-            <Col
-              key={`graduation-${index}`}
-              xs={24}
-              sm={12}
-              md={12}
-              lg={8}
-              xl={6}
+        {paginatedList.map((item, index) => (
+          <Col
+            key={`graduation-${index}`}
+            xs={24}
+            sm={12}
+            md={12}
+            lg={8}
+            xl={6}
+          >
+            <Link
+              href={`/${currentLanguage}/public/project/detail/${item.projectId}`}
             >
-              <Link
-                href={`/${currentLanguage}/public/project/detail/${item.projectId}`}
-              >
-                <CardItem item={item} />
-              </Link>
-            </Col>
-          ))}
+              <CardItem item={item} />
+            </Link>
+          </Col>
+        ))}
       </Row>
       <div className="flex justify-center items-center mt-6">
         <Pagination
+          current={currentPage}
           responsive={true}
           total={listProduct.length}
-          defaultPageSize={10}
+          pageSize={pageSize}
           showSizeChanger={false}
+          onChange={handlePageChange}
         />
       </div>
     </div>
